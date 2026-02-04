e88199f3efd7136e0064cbf7bb8a02222133d21b
Anand Reddy r <anandkumarreddy8456@gmail.com>
2026-02-04 18:13:48 +0530

    Initial commit with Maven project setup

---
+++ src/test/java/com/banking/digital/DigitalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
