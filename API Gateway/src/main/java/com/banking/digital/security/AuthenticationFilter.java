034a0d6db1952e862c8ca63cf3bb0f2d6292e93c
Anand Reddy r <anandkumarreddy8456@gmail.com>
2026-03-02 01:42:25 +0530

    Api gate has updated

--- API Gateway/src/main/java/com/banking/digital/security/AuthenticationFilter.java
+++ API Gateway/src/main/java/com/banking/digital/security/AuthenticationFilter.java
@@ -1,4 +1,48 @@
 package com.banking.digital.security;
 
-public class AuthenticationFilter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+@Component
+public class AuthenticationFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getURI().getPath();
+
+        // Allow auth endpoints
+        if (path.contains("/api/auth")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        if (!jwtUtil.validateToken(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
 }
--- API Gateway/src/main/java/com/banking/digital/security/JwtUtil.java
+++ API Gateway/src/main/java/com/banking/digital/security/JwtUtil.java
@@ -1,4 +1,18 @@
 package com.banking.digital.security;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+    private final String SECRET_KEY = "bankingSecretKey123456789012345678908765434567";
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
