715235d9e645344b90966366d9b952fc4a18cd68
Anand Reddy r <anandkumarreddy8456@gmail.com>
2026-03-01 19:55:09 +0530

    eureka details ass added

--- EUREKA-SERVER/src/test/java/com/banking/digital/EurekaServerApplicationTests.java
+++ EUREKA-SERVER/src/test/java/com/banking/digital/EurekaServerApplicationTests.java
@@ -3,11 +3,10 @@ package com.banking.digital;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EurekaServerApplication.class)
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
