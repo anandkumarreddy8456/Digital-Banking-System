edef484c602da64a302f145af18fec4929feff0e
Anand Reddy r <anandkumarreddy8456@gmail.com>
2026-03-10 08:55:30 +0530

    Transaction from account and to account validated

--- Transaction Service/src/main/java/com/banking/digital/common/ConstantMessages.java
+++ Transaction Service/src/main/java/com/banking/digital/common/ConstantMessages.java
@@ -9,4 +9,8 @@ public class ConstantMessages {
     public static final String properFromAccount="PLEASE PROVIDE FROM ACCOUNT NUMBER";
     public static final  String invalidDeposit="INVALID TO ACCOUNT";
     public static final String properToAccount="PLEASE PROVIDE TO ACCOUNT NUMBER";
+    public static final String transactionNotFound="TRANSACTION NOT FOUND";
+    public static final String internalServerError="INTERNAL SERVER ERROR";
+    public static final String invalidAccount="INVALID ACCOUNT NUMBER";
+
 }
--- Transaction Service/src/main/java/com/banking/digital/service/TransactionService.java
+++ Transaction Service/src/main/java/com/banking/digital/service/TransactionService.java
@@ -16,8 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -38,14 +40,27 @@ public class TransactionService {
         if (repository.existsByReferenceId(request.getReferenceId())) {
             return new ResponseEntity<>(new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), ConstantMessages.duplicateTrsReq,ConstantMessages.transactionId),HttpStatus.BAD_REQUEST);
         }
+        try {
+            String fromAccountNumber = null;
+            String toAccountNumber = null;
 
-        Transaction txn = new Transaction();
-        txn.setReferenceId(request.getReferenceId());
-        txn.setFromAccount(request.getFromAccount());
-        txn.setToAccount(request.getToAccount());
-        txn.setAmount(request.getAmount());
-        txn.setStatus(TransactionStatus.INITIATED);
-        repository.save(txn);
+            ApiResponse<String> fromResponse = accountClient.getAccountNumberByAccountNumber(request.getFromAccount());
+            if (fromResponse != null) {
+                fromAccountNumber = fromResponse.getApiResponse();
+            } else {
+                return new ResponseEntity<>(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ConstantMessages.invalidWithdraw,ConstantMessages.properFromAccount),HttpStatus.NOT_FOUND);
+            }
+
+            ApiResponse<String> toResponse = accountClient.getAccountNumberByAccountNumber(request.getToAccount());
+            if (toResponse != null) {
+                toAccountNumber = toResponse.getApiResponse();
+            } else {
+                return new ResponseEntity<>(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ConstantMessages.invalidDeposit,ConstantMessages.properToAccount),HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
 
         AccountTransactionRequest withdrawReq = new AccountTransactionRequest();
         withdrawReq.setAccountNumber(request.getFromAccount());
@@ -58,6 +73,13 @@ public class TransactionService {
         depositReq.setAmount(request.getAmount());
 
         accountClient.deposit(depositReq);
+        Transaction txn = new Transaction();
+        txn.setReferenceId(request.getReferenceId());
+        txn.setFromAccount(request.getFromAccount());
+        txn.setToAccount(request.getToAccount());
+        txn.setAmount(request.getAmount());
+        txn.setStatus(TransactionStatus.INITIATED);
+        repository.save(txn);
 
         txn.setStatus(TransactionStatus.SUCCESS);
         repository.save(txn);
@@ -68,9 +90,9 @@ public class TransactionService {
         notification.put("type", "EMAIL");
 
         notificationClient.sendNotification(notification);
-        return "Transaction Successful";
+        return new ResponseEntity<>(new ApiResponse<>(HttpStatus.ACCEPTED.value(),  ConstantMessages.transactionSuccess,ConstantMessages.transactionSuccess),HttpStatus.ACCEPTED);
     }
-    public String fallbackTransfer(TransferRequest request, Throwable ex) {
+    public ResponseEntity<ApiResponse<String>> fallbackTransfer(TransferRequest request, Throwable ex) {
 
         Transaction txn = new Transaction();
         txn.setReferenceId(request.getReferenceId());
@@ -81,6 +103,35 @@ public class TransactionService {
 
         repository.save(txn);
 
-        return "Transaction Failed";
+        return new ResponseEntity<>(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ConstantMessages.transactionFail,ConstantMessages.transactionFail),HttpStatus.NOT_FOUND);
+    }
+    public ResponseEntity<ApiResponse<Transaction>> getByReference(String referenceId){
+        try{
+            Transaction txt=repository.findByReferenceId(referenceId)
+                    .orElseThrow(() -> new RuntimeException(ConstantMessages.transactionNotFound));
+            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.ACCEPTED.value(), ConstantMessages.transactionSuccess,txt),HttpStatus.ACCEPTED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ConstantMessages.transactionNotFound,null),HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),ConstantMessages.internalServerError,null),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    public ResponseEntity<ApiResponse<List<Transaction>>> getByAccount(String accountNumber){
+        try{
+
+            String accNumber = null;
+
+            ApiResponse<String> fromResponse = accountClient.getAccountNumberByAccountNumber(accountNumber);
+            if (fromResponse != null) {
+                accNumber = fromResponse.getApiResponse();
+            } else {
+                return new ResponseEntity<>(new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ConstantMessages.invalidAccount,List.of()),HttpStatus.NOT_FOUND);
+            }
+            List<Transaction> lis=repository.findByFromAccountOrToAccount(accNumber, accountNumber);
+
+            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.ACCEPTED.value(), ConstantMessages.transactionSuccess,lis),HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),ConstantMessages.internalServerError,List.of()),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
